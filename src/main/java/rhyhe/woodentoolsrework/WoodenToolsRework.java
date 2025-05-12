d70f115af1bc1fd6f1c31cbecaddce6c93b876c5
name <143746324+Rhyhe@users.noreply.github.com>
2025-05-12 17:16:29 -0400

    unregisters stone tools

--- src/main/java/rhyhe/woodentoolsrework/WoodenToolsRework.java
+++ src/main/java/rhyhe/woodentoolsrework/WoodenToolsRework.java
@@ -1,38 +1,51 @@
 package rhyhe.woodentoolsrework;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.data.gamerule.TreecapitatorHelper;
+import net.minecraft.core.data.registry.Registries;
+import net.minecraft.core.data.registry.recipe.RecipeGroup;
+import net.minecraft.core.data.registry.recipe.entry.RecipeEntryCrafting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.RecipeBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-
-public class WoodenToolsRework implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+public class WoodenToolsRework implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint
+{
     public static final String MOD_ID = "woodentoolsrework";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
-    public void onInitialize() {
+    public void onInitialize()
+    {
         LOGGER.info("Wooden Tools Rework(woodentoolsrework) initialized.");
     }
 
-	@Override
-	public void onRecipesReady() {
-	}
+    @Override
+    public void onRecipesReady()
+    {
+        RecipeGroup<RecipeEntryCrafting<?, ?>> workbench = Registries.RECIPES.WORKBENCH;
+        workbench.unregister("stone_pickaxe");
+        workbench.unregister("stone_axe");
+        workbench.unregister("stone_sword");
+        workbench.unregister("stone_hoe");
+        workbench.unregister("stone_shovel");
+    }
 
-	@Override
-	public void initNamespaces() {
+    @Override
+    public void initNamespaces()
+    {
 
-	}
+    }
 
-	@Override
-	public void beforeGameStart() {
+    @Override
+    public void beforeGameStart()
+    {
 
-	}
+    }
 
-	@Override
-	public void afterGameStart() {
+    @Override
+    public void afterGameStart()
+    {
 
-	}
+    }
 }
