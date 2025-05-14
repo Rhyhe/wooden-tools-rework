b5e4fdcae3e4048c8c1985b2d79f14ad59603a75
name <143746324+Rhyhe@users.noreply.github.com>
2025-05-14 12:58:27 -0400

    bumped version to 1.0

--- src/main/java/rhyhe/woodentoolsrework/mixin/ToolMaterialMixin.java
+++ src/main/java/rhyhe/woodentoolsrework/mixin/ToolMaterialMixin.java
@@ -2,7 +2,6 @@ package rhyhe.woodentoolsrework.mixin;
 
 import net.minecraft.core.item.material.ToolMaterial;
 import org.spongepowered.asm.mixin.Debug;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import rhyhe.woodentoolsrework.Config;
@@ -13,7 +12,6 @@ public class ToolMaterialMixin
 {
     // This is probably a hack way to change the properties of wood to stone
     // - Rhyhe
-    @Final
     @Shadow
     @SuppressWarnings("unused")
     public final static ToolMaterial wood = Config.config.getBoolean("wood_is_stone") ?
