8c9c93a216fb33cd95ca0badb8f3aa3cce3ac4cb
name <143746324+Rhyhe@users.noreply.github.com>
2025-05-14 12:36:55 -0400

    changed some descriptions

--- src/main/java/rhyhe/woodentoolsrework/Config.java
+++ src/main/java/rhyhe/woodentoolsrework/Config.java
@@ -55,11 +55,12 @@ public class Config
     private static Toml createDefaultTOML()
     {
         return new Toml("Wooden Tool Rework Configuration File")
-                .addEntry("unregister", "Unregisters the stone tool recipes.", true)
-                .addEntry("wood_is_stone", "Replaces the wood material to stone, making all wooden tools stone tools.",
+                .addEntry("unregister", "Unregisters the stone tools recipes. This makes them uncraftable.", true)
+                .addEntry("wood_is_stone",
+                        "Replaces the wood material to stone, making wooden tools have the same properties as stone tools",
                         true)
                 .addEntry("build_better_pickaxe_iron",
-                        "Replaces the logo of the achievement BUILD_BETTER_PICKAXE with an iron pickaxe instead of a stone one.",
+                        "Replaces the logo of the achievement BUILD_BETTER_PICKAXE with an iron pickaxe instead of a stone pickaxe.",
                         true);
     }
 }
